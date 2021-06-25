9667ad230928e9c821a2c1bc5d9dbd4e3b92e185
sayampy <81681676+sayampy@users.noreply.github.com>
2021-06-25 12:43:09 +0530

    Update cli.py

--- py/cli.py
+++ py/cli.py
@@ -4,7 +4,7 @@ from os import system as sys
 @click.command()
 @click.argument('file_name',required=False,type=click.Path())
 def cli(file_name=None):
-    print('Running..')
+    #print('Running..')
     if file_name==None:
        file_name=r''
     sys('python {0}'.format(file_name))
