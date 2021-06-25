c0d30c5937f2e1146f205a16b84af8737f50f325
sayampy <81681676+sayampy@users.noreply.github.com>
2021-06-25 12:25:35 +0530

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,11 @@
+import click
+from os import system as sys
+
+@click.command()
+@click.argument('file_name',required=False,type=click.Path())
+def cli(file_name=None):
+    print('Running..')
+    if file_name==None:
+       file_name=r''
+    sys('python {0}'.format(file_name))
+
